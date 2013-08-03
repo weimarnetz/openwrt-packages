63e12426fe6aac71966c438e8c5d70b8cca7110a
Andi Bräu <ab@andi95.de>
2013-08-03 16:30:51 +0200

    weimar-splash: more i18n

--- utils/luci-weimar-splash/luasrc/controller/weimarnetz/splash.lua
+++ utils/luci-weimar-splash/luasrc/controller/weimarnetz/splash.lua
@@ -2,7 +2,7 @@ module("luci.controller.weimarnetz.splash", package.seeall)
 
 function index()
    page = node("freifunk", "captive")
-   page.title    = "Informationsseite"
+   page.title    = luci.i18n.translate("Informationsseite")
    page.order    = 1
    page.target   = template("weimarnetz-splash/infopage")
    page.setuser  = false
